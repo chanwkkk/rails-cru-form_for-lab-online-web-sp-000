442eb6918eae4693473deb1b6b3a035579cbdf12
Yingqi Chen <chanwk18@hotmail.com>
2019-09-18 07:00:07 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,30 @@
 class SongsController < ApplicationController
   def show
+    @song=Song.find(params[:id])
+  end
+
+  def new
+    @song=Song.new
+  end
+
+  def edit
+    @song=Song.find(params[:id])
+  end
+
+  def update
+    @song=Song.find(params[:id])
+    @song.update(genre_params)
+    redirect_to song_path(@song)
+  end
+
+  def create
+    @song=Song.create(genre_params)
+    redirect_to song_path(@song)
+  end
+Song
+  private
+
+  def song_params
+    params.require(:song).permit(:name)
   end
 end
