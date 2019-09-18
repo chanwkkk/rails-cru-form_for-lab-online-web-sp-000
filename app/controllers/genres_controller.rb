2c1e1feea5f6a7f228c00d690074b341321d0748
Yingqi Chen <chanwk18@hotmail.com>
2019-09-18 06:52:28 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,31 @@
 class GenresController < ApplicationController
+  def show
+    @genre=Artist.find(params[:id])
+  end
+
+  def new
+    @genre=Artist.new
+  end
+
+  def edit
+    @genre=Artist.find(params[:id])
+  end
+
+  def update
+    @genre=Artist.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to artist_path(@genre)
+  end
+
+  def create
+    @artist=Artist.create(genre_params)
+    redirect_to artist_path(@genre)
+  end
+
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name,:bio)
+  end
 end
